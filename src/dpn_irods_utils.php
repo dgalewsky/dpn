e82faa3761adfdfd4293c63d58853660da5b23b7
root <root@ip-10-151-98-92.ec2.internal>
2014-03-25 11:40:42 -0500

    Added new_dpn_irods_transfer db method

--- src/dpn_irods_utils.php
+++ src/dpn_irods_utils.php
@@ -1,7 +1,31 @@
 <?php
+
+require_once 'KLogger.php';
+require_once 'common.php';
+
+
+$log = new KLogger($_SERVER["DPN_HOME"] . "/log/dpn_log.txt", KLogger::INFO);
+
+function new_dpn_irods_transfer($dpn_object_id) {
+	$db = db_connect();
+	
+	$db->exec("insert into dpn_irods_transfer(dpn_object_id) values ($dpn_object_id)");
+		
+}
+
+
+
+
+
+
+
+
+
+
 // include the streamer class, which enable PHP core to recongnize "irods"
 // as a valid stream, just like file stream, socket stream, or HTTP stream.
 
+/*
 require_once("/home/dg2226/irodsphp/prods/src/Prods.inc.php");
 
 function dpn_puts($filename, $str) {
@@ -16,4 +40,6 @@ function dpn_puts($filename, $str) {
 
 dpn_puts("dg_test", "2015 Production system at TB scale");
 
+*/
 
+function 
