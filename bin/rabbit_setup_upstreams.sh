295d2fbfd6fb8953d02806763b0cf64166bde40f
dan galewsky <dgalewsky@austin.utexas.edu>
2014-06-19 11:43:27 -0500

    Clean up rabbit setup Z

---
+++ bin/rabbit_setup_upstreams.sh
@@ -0,0 +1,2 @@
+rabbitmqctl set_parameter federation-upstream-set dpn-upstreams '[ {"upstream":"sdr-upstream","exchange":"dpn-control-exchange"}, {"upstream":"chron-upstream","exchange":"dpn-control-exchange"}, {"upstream":"aptrust-upstream","exchange":"dpn-control-exchange"}, {"upstream":"ht-upstream","exchange":"dpn-control-exchange"}  ]'
+
--- bin/rabbitmq_setup.sh
+++ bin/rabbitmq_setup.sh
@@ -26,19 +26,20 @@ rabbitmqctl stop
 /etc/init.d/rabbitmq start
 #service rabbitmq-server restart
 
-rabbitmqctl set_parameter federation-upstream-set dpn-upstreams '[{"upstream":"sdr-upstream","exchange":"dpn-control-exchange"}]'
+
+# Set up the federation upstreams
+
+rabbitmqctl set_parameter federation-upstream apt-upstream '{"uri":"amqp://dpn-dev.aptrust.org"}'
 rabbitmqctl set_parameter federation-upstream chron-upstream '{"uri":"amqp://adapt-mq.umiacs.umd.edu"}'
 rabbitmqctl set_parameter federation-upstream ht-upstream '{"uri":"amqp://dpn.hathitrust.org"}'
+rabbitmqctl set_parameter federation-upstream sdr-upstream '{"uri":"amqp://dpn-demo.stanford.edu"}'
+
 
 echo "done set_parameter federation-upstream"
 
-rabbitmqctl set_parameter federation-upstream-set dpn-upstreams '[\
-{"upstream":"sdr-upstream","exchange":"dpn-control-exchange"}, \
-{"upstream":"chron-upstream","exchange":"dpn-control-exchange"}, \
-{"upstream":"ht-upstream","exchange":"dpn-control-exchange"} \
-]'
+rabbitmqctl set_parameter federation-upstream-set dpn-upstreams '[ {"upstream":"sdr-upstream","exchange":"dpn-control-exchange"}, {"upstream":"chron-upstream","exchange":"dpn-control-exchange"}, {"upstream":"aptrust-upstream","exchange":"dpn-control-exchange"}, {"upstream":"ht-upstream","exchange":"dpn-control-exchange"}  ]'
 
-rabbitmqctl set_parameter federation local-nodename '"utdr-node"'
+rabbitmqctl set_cluster_name 'utdr-node'
 
 rabbitmqctl set_policy federate-me "dpn-control-exchange" '{"federation-upstream-set":"dpn-upstreams"}'
 
